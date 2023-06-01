c534a94442052a0cf1f4e58e9903d02bf5ada32c
sriram-sahu <sriramsahu510@gamail.com>
2023-06-01 21:23:47 +0530

    seventh commit

--- src/components/AdminComponent/GetStudentDetails.js
+++ src/components/AdminComponent/GetStudentDetails.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
 import { useNavigate } from "react-router-dom";
 import { gapi } from "gapi-script";
 import jsPDF from "jspdf";
@@ -15,6 +16,16 @@ const GetStudentsDetails = () => {
   const [filteredData, setFilteredData] = useState(sheetData);
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
+  const [scores, setScores] = useState({
+    java: 0,
+    react: 0,
+    js: 0,
+    ma: 0,
+  });
+  const [streams, setStreams] = useState({
+    interests: 0,
+    aptitude: 0,
+  });
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -100,6 +111,85 @@ const GetStudentsDetails = () => {
 
     loadGoogleAPI();
   }, []);
+
+  const fetchingdat = () => {
+    let java = 0;
+    let ma_order = JSON.parse(process.env.REACT_APP_MA_ORDER);
+    let ma_answers = JSON.parse(process.env.REACT_APP_MA_ANSWERS);
+    let js = 0;
+    let javapercentage = 0;
+    let Napercentage = 0;
+    let jsPercentage = 0;
+    let reactPercentage = 0;
+    let mentalability = 0;
+    let react_score = 0;
+    sheetData.map((item, index) => {
+      Object.keys(item).map((score, i) => {
+        if (i > 2) {
+          if (
+            item[score] === "TRUE" &&
+            JSON.parse(process.env.REACT_APP_JAVASCRIPT_ORDER)
+              .map((item) => parseInt(item))
+              .includes(i - 2)
+          ) {
+            java += 1;
+            javapercentage = (java / sheetData.length / 5) * 100;
+          }
+          if (ma_order.map((item) => parseInt(item)).includes(i - 2)) {
+            if (typeof ma_answers[ma_order.indexOf(i - 2)] === "string") {
+              if (
+                item[score].toLowerCase() ===
+                ma_answers[ma_order.indexOf(i - 2)].toLowerCase()
+              ) {
+                mentalability += 1;
+              }
+            } else {
+              if (
+                parseInt(item[score]) === ma_answers[ma_order.indexOf(i - 2)]
+              ) {
+                mentalability += 1;
+              }
+              Napercentage = (mentalability / sheetData.length / 5) * 100;
+            }
+          }
+          if (
+            item[score] === "TRUE" &&
+            JSON.parse(process.env.REACT_APP_JAVASCRIPT_ORDER)
+              .map((item) => parseInt(item))
+              .includes(i - 2)
+          ) {
+            js += 1;
+            jsPercentage = (js / sheetData.length / 5) * 100;
+          }
+          if (
+            item[score] === "TRUE" &&
+            JSON.parse(process.env.REACT_APP_REACT_ORDER)
+              .map((item) => parseInt(item))
+              .includes(i - 2)
+          ) {
+            react_score += 1;
+            reactPercentage = (react_score / sheetData.length / 5) * 100;
+          }
+        }
+      });
+    });
+    setScores({
+      ...scores,
+      java: javapercentage,
+      react: reactPercentage,
+      ma: Napercentage,
+      js: jsPercentage,
+    });
+    setStreams({
+      ...streams,
+      interests: (javapercentage + reactPercentage + jsPercentage) / 3,
+      aptitude: Napercentage,
+    });
+    //
+  };
+  useEffect(() => {
+    fetchingdat();
+  }, [sheetData]);
 
   sheetData.map((item, index) => {
     let Java_Score = 0;
@@ -193,15 +283,48 @@ const GetStudentsDetails = () => {
     authInstance.signOut();
   };
 
+  const pieData = [
+    {
+      name: "Java",
+      value: scores.java,
+    },
+    {
+      name: "React",
+      value: scores.react,
+    },
+    {
+      name: "JavaScript",
+      value: scores.js,
+    },
+    {
+      name: "Mental Ability",
+      value: scores.ma,
+    },
+  ];
+  const pieData1 = [
+    {
+      name: "Interests",
+      value: streams.interests,
+    },
+    {
+      name: "Aptitude",
+      value: streams.aptitude,
+    },
+  ];
+  const COLORS = ["#8884d8", "#82ca9d", "#FFBB28", "#FF8042", "#AF19FF"];
+  const COLORS1 = ["#8884d8", "#82ca9d"];
+
   return (
     <div>
       <div>
         <p>
           {isSignedIn ? (
-            <span className='display-between'>
-              Email : {userEmail}{" "}
-              <button onClick={handleSignOut}>Sign Out</button>
-            </span>
+            <>
+              <span className='display-between'>
+                Email : {userEmail}{" "}
+                <button onClick={handleSignOut}>Sign Out</button>
+              </span>
+            </>
           ) : (
             <div className='display-column'>
               <h2>Login With Google</h2>
@@ -218,6 +341,56 @@ const GetStudentsDetails = () => {
       </div>
       {isSignedIn && sheetData.length > 0 && (
         <div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-around",
+              marginTop: "20px",
+            }}
+          >
+            <PieChart width={730} height={300}>
+              <Pie
+                data={pieData}
+                color='#000000'
+                dataKey='value'
+                nameKey='name'
+                cx='50%'
+                cy='50%'
+                outerRadius={120}
+                fill='#8884d8'
+              >
+                {pieData.map((entry, index) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index % COLORS.length]}
+                  />
+                ))}
+              </Pie>
+              <Tooltip />
+              <Legend />
+            </PieChart>
+            <PieChart width={730} height={300}>
+              <Pie
+                data={pieData1}
+                color='#000000'
+                dataKey='value'
+                nameKey='name'
+                cx='50%'
+                cy='50%'
+                outerRadius={120}
+                fill='#8884d8'
+              >
+                {pieData.map((entry, index) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS1[index % COLORS1.length]}
+                  />
+                ))}
+              </Pie>
+              <Tooltip />
+              <Legend />
+            </PieChart>
+          </div>
           <div className='display-center'>
             <div className='display-between'>
               Start Date:{" "}
@@ -240,7 +413,7 @@ const GetStudentsDetails = () => {
           </div>
           <div>
             {Array.isArray(sheetData) && filteredData.length > 0 ? (
-              <StudentsTable data={sheetData} />
+              <StudentsTable data={filteredData} />
             ) : (
               <div>No data available</div>
             )}
