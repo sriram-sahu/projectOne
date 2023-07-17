83d8777358fcb00a08f19a9d980ae3f45bb18d5d
praveenlingamsetti <praveenlingamsetti333@gmail.com>
2023-07-17 14:37:31 +0530

    cc3

--- src/components/Student/Test.js
+++ src/components/Student/Test.js
@@ -8,10 +8,7 @@ const Text = () => {
       <div className="details-container">
         <iframe
           src="https://docs.google.com/forms/d/e/1FAIpQLSc6Awak0_3NAajQr7wKL2Rs_Uchp6wbmzQSmeiobQHPoe0idg/viewform?embedded=true"
-          width="1000"
-          height="5200"
           title="test"
-          scrolling="no"
         >
           Loading…
         </iframe>
