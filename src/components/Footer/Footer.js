d2768a0c9d51cedd554c0042bd751681ebe46e6f
praveenlingamsetti <praveenlingamsetti333@gmail.com>
2023-07-18 13:12:45 +0530

    cc7

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,40 +3,40 @@ import "./Footer.css";
 import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
 const Footer = () => {
   return (
-    <div className='main'>
-      <div className='footer-main-container'>
-        <div className='social_media mt-2'>
+    <div className="main">
+      <div className="footer-main-container">
+        <div className="social_media mt-2">
           <FaFacebook style={{ fontSize: "30px", color: "#FFFFFF" }} />
           <FaInstagram style={{ fontSize: "30px", color: "#FFFFFF" }} />
           <FaLinkedin style={{ fontSize: "30px", color: "#FFFFFF" }} />
           <FaTwitter style={{ fontSize: "30px", color: "#FFFFFF" }} />
         </div>
       </div>
-      <hr className='my-hr' />
-      <div className='below-container'>
-        <div className='sb__footer-copyright'>
+      <hr className="my-hr" />
+      <div className="below-container">
+        <div className="sb__footer-copyright">
           <p>
             @{new Date().getFullYear()} STUDY GLOBAL{" "}
             <span> Overseas Education Consultants.</span> All right reserved.
           </p>
         </div>
-        <div className='sb__footer-below-links'>
-          <a>
+        <div className="sb__footer-below-links">
+          <a className="anchor" hef="#">
             <div>
               <p>Terms & Conditions</p>
             </div>
           </a>
-          <a>
+          <a className="anchor" hef="#">
             <div>
               <p>Privacy</p>
             </div>
           </a>
-          <a>
+          <a className="anchor" hef="#">
             <div>
               <p>About</p>
             </div>
           </a>
-          <a>
+          <a className="anchor" hef="#">
             <div>
               <p>Contact</p>
             </div>
