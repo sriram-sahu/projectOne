421854f5eb66b8a3ce36674c62f251177c7ab02d
praveenlingamsetti <praveenlingamsetti333@gmail.com>
2023-07-19 17:52:20 +0530

    cc9

--- src/components/Admin/SendAssessments.js
+++ src/components/Admin/SendAssessments.js
@@ -116,8 +116,11 @@ function SendAssessments(props) {
   // triggers when send button clicked
   const handleSendEmail = () => {
     const subject = "Stream Recommendation Test from Study Global"; // email subject
+    const imageUrl =
+      "https://res.cloudinary.com/de5cu0mab/image/upload/v1688971136/Logo_Final_uovjgi.png";
     const body =
-      "Hello Students, %0D%0A %0D%0AGreetings from Study Global. Below is the link for you to give the Stream Recommendation Test, which will help you choose a career path that suits your aptitude and interests.%0D%0A  %0D%0AHere is the link: https://study-global.netlify.app/studentLogin %0D%0A %0D%0A %0D%0A Thank You,%0D%0A %0D%0A Study Global Team  %0D%0A %0D%0A %0D%0A ";
+      "Hello Students, %0D%0A  %0D%0AGreetings from Study Global. Below is the link for you to give the Stream Recommendation Test, which will help you choose a career path that suits your aptitude and interests. %0D%0A %0D%0AHere is the link: https://study-global.netlify.app/studentLogin %0D%0A  %0D%0A Thank You, %0D%0A %0D%0A Study Global Team  %0D%0A  %0D%0A " +
+      `&lt;img src="${imageUrl}" alt="Image" /&gt;`;
     // send mail when file format is txt, docx, xls, xlsx
     if (
       fileExtension === "txt" ||
